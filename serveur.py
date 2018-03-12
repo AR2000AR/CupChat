37f52e8039a5130eb437140305ec8297399be969
AR2000 <AR2000AR@users.noreply.github.com>
2018-03-12 15:41:06 +0100

    Add intelligence

--- serveur.py
+++ serveur.py
@@ -1,13 +1,16 @@
 from socketTools import *
 from tools import *
 from socket import *
+from account import *
 import threading
 #=============================
 class ClientThread(threading.Thread):
-    def __init__(self,client):
+    def __init__(self,client,account,clients):
         threading.Thread.__init__(self)
         self.client = client
         self.connected = True
+        self.account=account
+        self.clients=clients
         
     def run(self):
         try:
@@ -21,30 +24,59 @@ class ClientThread(threading.Thread):
                     break
                 #------------------------------
                 else:
-                    self.client.send(bytes(msg,"UTF-8"))
-                    print(msg)
+                    msg=msg.split(";")
+                    if msg[0]=="<|ACCOUNT|>":
+                        if msg[1]=="<|AUTH|>":
+                            tmp=self.account.check(msg[2],msg[3])
+                            if tmp==True:
+                                self.client.send(b'<|AUTH|>;<|ACCEPTED|>')
+                            else:
+                                client.send(b'<|AUTH|>;<|REJECTED|>')
+                        elif msg[1]=="<|CREATE|>":
+                            if self.account.crate(msg[2],msg[3])==True:
+                                self.client.send(b'<|ACCOUNT|>;<|CREATE|>;DONE')
+                            else:
+                                self.client.send(b'<|ACCOUNT|>;<|CREATE|>;EXIST')
+                    elif msg[0]=="<|MESSAGE|>":
+                        self.clients.sendAll(bytes(msg[1]+";"+msg[2],"UTF-8"))
         except ConnectionResetError:
             self.connected = False
-            self.stop
+            pass
         else:
-            self.stop()
+            pass
+#-----------------------------
+class MultiClient():
+    def __init__(self):
+        self._clientList=[]
+
+    def addClient(self,client):
+        self.clientList.append(client)
+
+    def sendAll(self,msg):
+        for client in self._clientList:
+            client.send(bytes(msg,"UTF-8"))
 #=============================
 def init():
     server = socket(AF_INET, SOCK_STREAM)
     server.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
     server.bind((myip(),1948))
+    account = Account("account.db")
+    clients = MultiClient()
+    if account.statu()=="No file":
+        raise FileNotFoundError("No account.db file")
     print(myip())
     print("1948")
-    return server
+    return server,account,clients
 #===============================================
 #===============================================
-server = init()
+server,account,clients = init()
 while True:
     #################################################
     while True:#Connecte le client
         client,adresse = serverAccept(server)
         if client!=False:
             print("Connection to a client")
-            newthread = ClientThread(client)
+            newthread = ClientThread(client,account,clients)
+            clients.addClient(client)
             newthread.start()
     
